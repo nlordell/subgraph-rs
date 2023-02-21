0051a74da347f21fce0411f7bef35d3e3781749f
Jose Alvarez <jp.alvarezl@gmail.com>
2023-02-20 16:51:33 -0800

    Added TODO for wasm target

--- cargo-subgraph/src/commands.rs
+++ cargo-subgraph/src/commands.rs
@@ -5,12 +5,12 @@ use std::collections::HashMap;
 use crate::models::Params;
 use std::env;
 
-pub fn param_validation(params: &Params) -> Result<(), Box<dyn std::error::Error>> { 
+pub fn param_validation(params: &Params) -> Result<(), Box<dyn std::error::Error>> {
     let _graph_slug = params.graph_slug.to_owned().unwrap_or_else(|| {
         env::var("GRAPH_SLUG")
             .expect("You can pass the option --graph-slug or set the env variable GRAPH_SLUG")
     });
-    
+
     let _graph_studio_token = params.graph_studio_token.to_owned().unwrap_or_else(|| {
         env::var("GRAPH_STUDIO_TOKEN")
             .expect("You can pass the option --graph-studio-token or set the env variable GRAPH_STUDIO_TOKEN")
@@ -30,15 +30,16 @@ pub fn cargo_compile(project_name: &str, release: &bool) {
         command.arg("--release");
     }
 
+    // TODO: add a check before attempting compiling for this target. The resulting error is very cryptic.
     command.arg("--target")
         .arg("wasm32-unknown-unknown");
 
     let output = command.output().expect("Build failure");
-    
+
     println!("status: {}", output.status);
     io::stdout().write_all(&output.stdout).unwrap();
     io::stderr().write_all(&output.stderr).unwrap();
-    
+
     assert!(output.status.success());
 
     add_ipfs("file_name").expect("Adding to ipfs failure");
